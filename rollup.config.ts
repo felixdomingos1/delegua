467bbdba187c3b159d9e2ff90234e8c3b3acfc66
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-05-05 10:43:29 -0700

    - Anotações sobre o uso de Rollup; - Substituição do método de geração do UMD com Browserify.

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,6 +5,14 @@ import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
+/**
+ * Nós tentamos usar o Rollup para gerar um UMD de Delégua, mas ele cria problemas com o `antlr4ts`.
+ * O método escolhido para gerar o UMD é o mesmo de `delegua-web`, usando o Browserify, que
+ * não cria problemas.
+ * 
+ * Essa configuração é mantida aqui porque pode ser interessante para outros projetos da Design
+ * Líquido que se beneficiem desse tipo de conhecimento.
+ */
 /** @type {import('rollup').RollupOptions} */
 export default {
     plugins: [
@@ -19,7 +27,9 @@ export default {
             sourceMap: true
         }),
         commonjs(),
-        resolve(),
+        resolve({
+            browser: true
+        }),
         babel({
             babelHelpers: 'bundled'
         }),
