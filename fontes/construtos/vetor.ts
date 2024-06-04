e9400ca8713a661fe5c2653e83711c606c8eee38
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-06-04 09:58:31 -0700

    Construto de vetor passa a armazenar informação do tamanho inicial.

--- fontes/construtos/vetor.ts
+++ fontes/construtos/vetor.ts
@@ -4,14 +4,20 @@ import { Construto } from './construto';
 export class Vetor implements Construto {
     linha: number;
     hashArquivo: number;
+    tamanho: number;
 
     valores: any[];
 
-    constructor(hashArquivo: number, linha: number, valores: any[]) {
+    constructor(hashArquivo: number, linha: number, valores: any[], tamanho?: number) {
         this.linha = linha;
         this.hashArquivo = hashArquivo;
 
         this.valores = valores;
+        if (tamanho) {
+            this.tamanho = tamanho;
+        } else {
+            this.tamanho = this.valores.length;
+        }
     }
 
     async aceitar(visitante: VisitanteComumInterface): Promise<any> {
